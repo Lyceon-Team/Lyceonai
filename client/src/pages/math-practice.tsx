3bdf75710c3b995f9f7f8abafd3c57173428338d
hforchu <harveyforchu@gmail.com>
2026-04-29 13:24:22 -0400

    fix: practice session filtering

--- client/src/pages/math-practice.tsx
+++ client/src/pages/math-practice.tsx
@@ -2,10 +2,13 @@ import { useMemo } from "react";
 import { useSearch } from "wouter";
 import CanonicalPracticePage from "@/components/practice/CanonicalPracticePage";
 import { parsePracticeDurationFromSearch } from "@/lib/practice-duration";
+import { parseDifficultiesFromSearch, parseDomainsFromSearch } from "@/lib/practice-filters";
 
 export default function MathPracticePage() {
   const search = useSearch();
   const targetMinutes = useMemo(() => parsePracticeDurationFromSearch(search), [search]);
+  const difficulties = useMemo(() => parseDifficultiesFromSearch(search), [search]);
+  const domains = useMemo(() => parseDomainsFromSearch(search), [search]);
 
   return (
     <CanonicalPracticePage
@@ -13,6 +16,8 @@ export default function MathPracticePage() {
       badgeLabel="Math"
       section="math"
       targetMinutes={targetMinutes}
+      difficulties={difficulties.length > 0 ? difficulties : undefined}
+      domains={domains.length > 0 ? domains : undefined}
     />
   );
 }
